b587312e82f700e7c247eed176b4926cd557b078
AdityaBahl <adityabahl12345@gmail.com>
2022-06-28 17:59:34 +0530

    Templates 4

---
+++ templates/templates_4/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+template <class t>
+class test
+{
+    t a,b;
+public:
+    void get()//def
+    {
+        cin>>a>>b;
+    }
+    t sum();//dec
+};
+template <class t>
+t test<t> ::sum()
+{
+    return a+b;
+}
+int main()
+{
+    test <int>t1;
+    test <float>t2;
+    cout<<"Enter 2 integers: "<<endl;
+    t1.get();
+    cout<<"sum of integers is: "<<t1.sum()<<endl;
+    cout<<"Enter 2 floats: "<<endl;
+    t2.get();
+    cout<<"sum of floats is: "<<t2.sum()<<endl;
+    return 0;
+}
