4be05b5c8d847adc92d73f51fa6f0f161946e162
AdityaBahl <adityabahl12345@gmail.com>
2022-06-29 21:33:24 +0530

    Alternate version of Numbered Triangle 2

--- patterns/alt_numbered_triangle_2/main.cpp
+++ patterns/alt_numbered_triangle_2/main.cpp
@@ -1,17 +1,18 @@
+//
 #include <iostream>
 using namespace std;
 int main()
 {
-    int i,j,n,cnt=1;
-    cin>>n;
-    for(i=1;i<=n;i++)
+    int i, j, n, cnt = 1;
+    cin >> n;
+    for (i = 1; i <= n; i++)
     {
-        for(j=1;j<=i;j++)
-    {
-        cout<<cnt<<" ";
-        cnt++;
-    }
-    cout<<endl;
+        for (j = 1; j <= i; j++)
+        {
+            cout << cnt << " ";
+            cnt++;
+        }
+        cout << endl;
     }
     return 0;
 }
