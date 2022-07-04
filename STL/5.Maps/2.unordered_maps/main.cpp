4784342d1c2d5a4bc83607caa2d32d52dff28361
AdityaBahl <adityabahl12345@gmail.com>
2022-07-04 20:50:36 +0530

    Unordered maps

---
+++ STL/5.Maps/2.unordered_maps/main.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>                   //same as 1.) maps, but in unordered, it saves time complexity(O(1) instead of O(N)) and presents list in no order
+//#include <map>                        // some things cant be used in unordered maps like vectors and maps
+#include <bits/stdc++.h>
+using namespace std; // keys are stored in sorted order of their keys
+int main()           // all keys should be unique
+{                    // if key repeated, latest value will be kept, others replaced
+    unordered_map<int, string> m;
+    m[1] = "abc"; // O(log(n)) time complexity
+    m[5] = "cdb";
+    m[3] = "adc";
+    m.insert({4, "afg"}); // alternate way to input value in map
+    unordered_map<int, string>::iterator it;
+    for (it = m.begin(); it != m.end(); it++)
+        cout << it->first << " " << it->second << endl;
+    cout << endl;
+    // cout<<m.first<<"  "<<m.second;
+    for (auto &pr : m)
+        cout << pr.first << " " << pr.second << endl;
+    auto iter = m.find(2); // to find a value(by key)
+    if (iter == m.end())
+        cout << "No Value" << endl;
+    else
+        cout << iter->first << "  " << iter->second;
+    cout << endl;
+    m.erase(1); // to delete
+    iter = m.find(3);
+    m.erase(iter); // alternate way to delete
+    for (it = m.begin(); it != m.end(); it++)
+        cout << it->first << " " << it->second << endl;
+    m.clear(); // to delete the map
+    return 0;
+}
