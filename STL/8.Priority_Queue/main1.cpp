eac62a8c43c6f239571e85c279cbd189fe3539d6
AdityaBahl <adityabahl12345@gmail.com>
2022-07-05 06:33:10 +0530

    Priority Queue Basic

--- STL/8.Priority_Queue/main1.cpp
+++ STL/8.Priority_Queue/main1.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 int main()
 {
     priority_queue<int> pq;
+    cout << "The Vector before pq was:";
     vector<int> vec = {10, 5, 15, 2, 1, 9, 50};
     for (int i = 0; i < vec.size(); i++)
     {
+        pq.push(vec[i]);
     }
+    cout << "The Priority Queue is: ";
 }
