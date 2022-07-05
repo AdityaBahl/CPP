2e2c25826e8303fbb72af1a72e9e5afba1b686e1
AdityaBahl <adityabahl12345@gmail.com>
2022-07-05 06:31:05 +0530

    Priority Queue Basic

---
+++ STL/8.Priority_Queue/main1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    priority_queue<int> pq;
+    vector<int> vec = {10, 5, 15, 2, 1, 9, 50};
+    for (int i = 0; i < vec.size(); i++)
+    {
+    }
+}
