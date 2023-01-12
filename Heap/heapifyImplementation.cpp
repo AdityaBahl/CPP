a74828c9b3d2a7dec97eccea655d2eb9ff143fbc
AdityaBahl <adityabahl12345@gmail.com>
2023-01-12 18:06:48 +0530

    Heapify Code

---
+++ Heap/heapifyImplementation.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+class heap
+{
+public:
+    int arr[100];
+    int size;
+    heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+    void insert(int val)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+    if (left < n and arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+    if (right < n and arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+int main()
+{
+    heap h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.print();
+    return 0;
+}
