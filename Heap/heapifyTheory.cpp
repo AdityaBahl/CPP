7e95f98b4eb311ffaaa36c26b108aefd478372f8
AdityaBahl <adityabahl12345@gmail.com>
2023-01-12 16:09:07 +0530

    Theory of Heapify

---
+++ Heap/heapifyTheory.cpp
@@ -0,0 +1,15 @@
+/*
+Note:
+1. Leaf Nodes of a Complete Binary Tree(CBT)(refer to heapTheory.cpp) lie
+   between n/2 to n where n = number of nodes.
+   For example - I/p = 54 53 55 52 50
+                Then, leaf nodes are from (n/2)+1 to n
+                i.e; (5/2)+1 to 5
+                i.e; 3 to 5
+
+Q. What is Heapify?
+Ans. Heapify is the process of creating a heap data structure from a
+    binary tree represented using an array. It is used to create Min-Heap
+    or Max-heap. Start from the first index of the non-leaf node whose
+    index is given by n/2 – 1.
+*/
