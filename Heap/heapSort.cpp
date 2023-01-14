f580dcf070fca93c65cc0717d12f29883e23f8ae
AdityaBahl <adityabahl12345@gmail.com>
2023-01-14 10:48:28 +0530

    Heap Sort

--- Heap/heapSort.cpp
+++ Heap/heapSort.cpp
@@ -92,6 +92,19 @@ void heapify(int arr[], int n, int i)
         heapify(arr, n, largest);
     }
 }
+void heapSort(int arr[], int n)
+{
+    int size = n;
+    // step 3
+    while (size > 1)
+    {
+        // step 1
+        swap(arr[size], arr[1]);
+        size--;
+        // step 2
+        heapify(arr, size, 1);
+    }
+}
 int main()
 {
     heap h;
@@ -103,11 +116,18 @@ int main()
     h.print();
     int arr[6] = {-1, 54, 53, 55, 52, 50};
     int n = 5; // size of array
+    // heap creation
     for (int i = n / 2; i > 0; i--)
     {
         heapify(arr, n, i);
     }
     cout << "printing array\n";
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    // heapSort(arr,n);
+    cout << "printing heapSort-ed array\n";
     for (int i = 1; i <= n; i++)
     {
         cout << arr[i] << " ";
