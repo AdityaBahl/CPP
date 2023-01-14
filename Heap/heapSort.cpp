1e25ad741e8764ffa87b2bd3763a06a18715e7da
AdityaBahl <adityabahl12345@gmail.com>
2023-01-14 10:35:06 +0530

    Heap Sort

--- Heap/heapSort.cpp
+++ Heap/heapSort.cpp
@@ -1,4 +1,20 @@
 /*
 Q. What is Heap Sort?
+Ans. Heap sort is an in-place algorithm.Its typical implementation is
+not stable, but can be made stable. Typically 2-3 times slower than
+well-implemented QuickSort.  The reason for slowness is a lack of
+locality of reference.
+
+Q. Advantages of Heap Sort?
 Ans.
+1. Efficiency –  The time required to perform Heap sort increases
+logarithmically while other algorithms may grow exponentially slower as
+the number of items to sort increases. This sorting algorithm is very
+efficient.
+2. Memory Usage – Memory usage is minimal because apart from what is
+necessary to hold the initial list of items to be sorted, it needs no
+additional memory space to work
+3. Simplicity –  It is simpler to understand than other equally efficient
+ sorting algorithms because it does not use advanced computer science
+ concepts such as recursion
 */
